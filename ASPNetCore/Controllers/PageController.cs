632633243d8be071f3599501b53abd1348f68dea
San Zayp <SanZayp@outlook.com>
2019-03-24 20:45:06 +0600

    Create_page_controller

---
+++ ASPNetCore/Controllers/PageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNetCore.Views.Page;
+
+namespace ASPNetCore.Controllers
+{
+    public class PageController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{action = Page}/{page:int}")]
+        public IActionResult Page(int page)
+        {
+            List<Page> pages = new List<Page>();
+            pages.Add(new Page(1, "First page"));
+            pages.Add(new Page(2, "Second page"));
+            Page _page = pages.Find((p) => p.Number == page);
+            if (_page != null)
+            {
+                ViewData["Page"] = _page.Number;
+                ViewData["Message"] = _page.Content;
+            }
+            return View();
+        }
+    }
+}
---
+++ ASPNetCore/Views/Page/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.Views.Page
+{
+    public class Page
+    {
+        public int Number { get; private set; }
+        public string Content { get; private set; }
+
+        public Page(int number, string content)
+        {
+            Number = number;
+            Content = content;
+        }
+
+
+    }
+}
