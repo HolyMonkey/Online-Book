6242c495dc014d0ef6a5bd7dcd89dd3742600a1b
San Zayp <SanZayp@outlook.com>
2019-03-24 21:01:53 +0600

    s

--- ASPNetCore/Startup.cs
+++ ASPNetCore/Startup.cs
@@ -54,9 +54,9 @@ namespace ASPNetCore
 
             app.UseMvc(routes =>
             {
-            routes.MapRoute(
-                name: "default",
-                template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
