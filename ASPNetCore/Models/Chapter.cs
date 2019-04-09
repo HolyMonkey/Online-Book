ff4e199b38c916fade3bdd1d0df5ac96025bd882
Nagornyy Artem <infallen@gmail.com>
2019-04-09 16:55:46 +0300

    Adding new class Chapter, new view Contents and output Contents

---
+++ ASPNetCore/Models/Chapter.cs
@@ -0,0 +1,20 @@
+﻿using ASPNetCore.Views.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.Models
+{
+    public class Chapter
+    {
+        public string Name { get; private set; }
+        public List<Page> Pages { get; private set; }
+
+        public Chapter(string name, List<Page> pages)
+        {
+            Name = name;
+            Pages = pages;
+        }
+    }
+}
